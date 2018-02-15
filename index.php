3d16db8e62c0eb2dd33eb0a98d010bcc72f37117
Lenath <kszal94@gmail.com>
2018-02-15 21:32:01 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pl">
+	<head>
+		<title>Polcern Sp. z o. o.</title>
+		
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+
+    	<link href="css/main.css" rel="stylesheet">
+	</head>
+
+	<body>
+		<?php include("navbar.php"); ?>
+		
+		<div id="home" class="bgimg-1">
+			<div>
+				<div style="display: table; height: 600px; overflow: hidden;">
+	              <div style="display: table-cell; vertical-align: middle;">
+	                <div class="text-logo">
+	                  Polcern
+	                </div>
+	                <div class="text-motto">
+	                  motto firmy
+	                </div>
+	             </div>
+	          </div>
+			</div>
+		</div>
+		<div>
+		<br><br><br><br>
+		Polcern Sp. z o. o.
+		</div>
+	</body>
+</html>
