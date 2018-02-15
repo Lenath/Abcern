56d3a1266774a83417aa3f72efad135f1b7f88e8
Shon2751 <dawidszal@gmail.com>
2018-02-16 00:10:48 +0100

    navbarfix
    poprawka navbara

--- navbar.php
+++ navbar.php
@@ -1,14 +1,14 @@
-		<header>
-	      <!-- pasek nawigacyjny -->
-	      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-	        <a class="navbar-brand" href="#home">Polcern</a>
-	        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-	          <span class="navbar-toggler-icon"></span>
-	        </button>
-	        <div class="collapse navbar-collapse" id="navbarCollapse">
-	          <ul class="navbar-nav mr-auto">
-	            <li class="nav-item">
-	              <a class="nav-link" href="#about">O nas <span class="sr-only">(current)</span></a>
+<header>
+	<!--PASEK NAWIGACYJNY-->
+	<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+		<a class="navbar-brand" href="index.php#home">Polcern</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarCollapse">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item">
+	              <a class="nav-link" href="index.php#about">O nas <span class="sr-only">(current)</span></a>
 	            </li>
 	            <li class="nav-item">
 	              <a class="nav-link" href="#">Projekty</a>
@@ -18,8 +18,8 @@
 	            </li>
 	            <li class="nav-item">
 	              <a class="nav-link" href="#">Kontakt</a>
-	            </li>
-	          </ul>
-	        </div>
-	      </nav>
-	    </header>
+				</li>
+			</ul>
+		</div>
+	</nav>
+</header>
