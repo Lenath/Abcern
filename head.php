0748bc670aca846e09a02bb1bbfad9b20c99a997
Shon2751 <dawidszal@gmail.com>
2018-02-15 23:46:57 +0100

    1.1.1
    1.1.1

---
+++ head.php
@@ -0,0 +1,13 @@
+<head>
+    <!-- TYTUŁ STRONY -->
+    <title><?php echo $res["title"]; ?></title>
+    <!-- OPIS STRONY -->
+    <meta name="description" content="<?php echo $res["description"]; ?>">
+    <!-- SŁOWA KLUCZOWE -->
+    <meta name="keywords" content="<?php echo $res["keywords"]; ?>">
+    <!-- AUTOR -->
+    <meta name="author" content="Klaudia Szal, Shonx">
+    <!-- STYLE -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+</head>
--- index.php
+++ index.php
@@ -4,20 +4,8 @@
 <?php $res = get_page("*", "main", $conn); ?>
 <!DOCTYPE html>
 <html lang="pl">
-	<head>
-		<!-- TYTUŁ STRONY -->
-		<title><?php echo $res["title"]; ?></title>
-		<!-- OPIS STRONY -->
-		<meta name="description" content="<?php echo $res["description"]; ?>">
-		<!-- SŁOWA KLUCZOWE -->
-		<meta name="keywords" content="<?php echo $res["keywords"]; ?>">
-		<!-- AUTOR -->
-		<title>Polcern Sp. z o. o.</title>
+<?php include("head.php"); ?>
 
-		<!-- STYLE -->
-		<link href="css/bootstrap.min.css" rel="stylesheet">
-    	<link href="css/main.css" rel="stylesheet">
-	</head>
 	<body>
 		<?php include("navbar.php"); ?>
 		
