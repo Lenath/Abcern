88b17b005ff0530267283a685a2818bb62ad67ee
Lenath <kszal94@gmail.com>
2018-02-16 03:26:50 +0100

    projekty hover

--- projects.php
+++ projects.php
@@ -12,10 +12,37 @@
 		<div class="container space">
 				<h1>PROJEKTY</h1>
 				<hr>
-				<div>
-
-				</div>
+			<div class="row">
+					<div class="col col-p thumbnail">
+						<a href="#" alt=""><img src="img/p1.jpg" class="project-img"></a>
+						<div class="description">		
+							Turystyka travelSafety
+						</div>
+					</div>
+					<div class="col col-p">
+						<a href="#" alt=""><img src="img/p2.jpg" class="project-img"></a>
+					</div>
+						
 			</div>
+			<div class="row">
+					<div class="col col-p">
+						<a href="#" alt=""><img src="img/p3.jpg" class="project-img"></a>
+					</div>
+					<div class="col col-p">
+						<a href="#" alt=""><img src="img/p4.jpg" class="project-img"></a>
+					</div>
+						
+			</div>
+			<div class="row">
+					<div class="col col-p">
+						<a href="#" alt=""><img src="img/p5.jpg" class="project-img"></a>
+					</div>
+					<div class="col col-p">
+						<a href="#" alt=""><img src="img/p6.jpg" class="project-img"></a>
+					</div>
+						
+			</div>
+		</div>
 
 		<!-- ZAŁĄCZENIE STOPKI I SKRYPTÓW -->
 		<?php include("footer.php"); ?>
