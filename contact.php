b8620c2cd0f2fc844f23e86e60566260c0270b57
Shon2751 <dawidszal@gmail.com>
2018-02-16 06:33:29 +0100

    mapa

--- contact.php
+++ contact.php
@@ -14,8 +14,32 @@
 				<hr>
 		</div>
 
+		<div class="container" id="contact" name="contact">
+			<div class="row">
+				<br>
+				<!-- MAPKA GOOGLA -->
+				<div id="map"></div>
+			</div>
+		</div>
+
 		<!-- ZAŁĄCZENIE STOPKI I SKRYPTÓW -->
 		<?php include("footer.php"); ?>
+		<script>
+			function initMap() {
+				var uluru = {lat: 50.0550132, lng: 19.9076992};
+				var map = new google.maps.Map(document.getElementById('map'), {
+					zoom: 14,
+					center: uluru
+				});
+				var marker = new google.maps.Marker({
+					position: uluru,
+					map: map
+				});
+			}
+		</script>
+		<script async defer
+				src="https://maps.googleapis.com/maps/api/js?key= AIzaSyB-ijRGhGramaD76XH5bAhBVfjxlWmkCto&callback=initMap">
+		</script>
 		<!-- ZAMKNIĘCIE POŁĄCZENIA Z BAZĄ -->
 		<?php mysqli_close($conn); ?>
 	</body>
