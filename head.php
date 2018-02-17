60efa3db8ad617abd9247c165b87c65353d2517f
Lenath <kszal94@gmail.com>
2018-02-17 20:35:40 +0100

    dfgh

--- head.php
+++ head.php
@@ -19,17 +19,17 @@
     <script>
     $(document).ready(function() { 
  
-    $('a[href^="#"]').on('click', function(event) {
-    
-        var target = $( $(this).attr('href') );
-    
-        if( target.length ) {
-            event.preventDefault();
-            $('html, body').animate({
-                scrollTop: target.offset().top
-            }, 1000);
-        }
-    });
- });
-</script>
+        $('a[href^="#"]').on('click', function(event) {
+        
+            var target = $( $(this).attr('href') );
+        
+            if( target.length ) {
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+            }
+        });
+     });
+    </script>
 </head>
