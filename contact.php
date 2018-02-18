461b6bc0fdea53f24f956e1f2b663bdf6166b765
Shon2751 <dawidszal@gmail.com>
2018-02-18 02:05:23 +0100

    skrypty

--- contact.php
+++ contact.php
@@ -84,6 +84,8 @@
 				src="https://maps.googleapis.com/maps/api/js?key= AIzaSyB-ijRGhGramaD76XH5bAhBVfjxlWmkCto&callback=initMap">
 		</script>
 		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://getbootstrap.com/assets/js/vendor/popper.min.js"></script>
 		<!-- ZAMKNIĘCIE POŁĄCZENIA Z BAZĄ -->
 		<?php mysqli_close($conn); ?>
 	</body>
